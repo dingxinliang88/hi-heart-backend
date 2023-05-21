dafdd37ab529ab91a3db8331bfb7c72460cf0b39
CodeJuzi <d1741530592@163.com>
2023-05-21 09:30:50 +0800

    add: delete tag && auth check

--- src/main/java/com/juzi/heart/service/impl/TagServiceImpl.java
+++ src/main/java/com/juzi/heart/service/impl/TagServiceImpl.java
@@ -11,7 +11,6 @@ import com.juzi.heart.mapper.TagMapper;
 import com.juzi.heart.model.dto.tag.TagAddRequest;
 import com.juzi.heart.model.dto.tag.TagEditRequest;
 import com.juzi.heart.model.entity.Tag;
-import com.juzi.heart.model.vo.tag.TagVO;
 import com.juzi.heart.model.vo.user.UserVO;
 import com.juzi.heart.service.TagService;
 import com.juzi.heart.service.UserService;
@@ -60,15 +59,12 @@ public class TagServiceImpl extends ServiceImpl<TagMapper, Tag>
         ThrowUtils.throwIf(parentId < 0, StatusCode.PARAMS_ERROR, "id不能小于0！");
         // 获取当前登录用户
         UserVO loginUser = userService.getLoginUser(request);
-        Integer hasChildren = HAS_NO_CHILDREN;
+        Integer hasChildren = HAS_CHILDREN;
         if (!DEFAULT_PARENT_ID.equals(parentId)) {
             // 如果不是添加的父标签，校验所属的父标签是否存在
             Tag tag = this.getById(parentId);
             ThrowUtils.throwIf(Objects.isNull(tag), StatusCode.NOT_FOUND_ERROR, "对应的父标签不存在");
-        } else {
-            // 添加的是父标签，只有管理员可以添加父标签
-            ThrowUtils.throwIf(!ADMIN.equals(loginUser.getUserRole()), StatusCode.NO_AUTH_ERROR, "你不能添加父标签！");
-            hasChildren = HAS_CHILDREN;
+            hasChildren = HAS_NO_CHILDREN;
         }
         // 插入数据
         Tag tag = new Tag();
@@ -101,38 +97,6 @@ public class TagServiceImpl extends ServiceImpl<TagMapper, Tag>
     }
 
     @Override
-    public List<TagVO> listTag() {
-        List<Tag> tagList = this.list();
-        // 分组，按照父标签名称 || 默认分组（存放所有的子标签）
-        Map<String, List<Tag>> parentTagMap = tagList.stream()
-                .collect(Collectors.groupingBy(tag -> {
-                    if (tag.getParentId() == 0) {
-                        return tag.getTagName();
-                    } else {
-                        // 根据父标签id获取父标签名称
-                        return tagList.stream()
-                                .filter(t -> t.getId().equals(tag.getParentId()))
-                                .map(Tag::getTagName)
-                                .findFirst().orElse(DEFAULT_GROUP);
-                    }
-                }));
-
-        List<TagVO> result = new ArrayList<>();
-        for (String parentTagName : parentTagMap.keySet()) {
-            List<Tag> childrenTags = parentTagMap.get(parentTagName);
-            List<String> childTagNameList = childrenTags.stream()
-                    .map(Tag::getTagName)
-                    .collect(Collectors.toList());
-            TagVO tagVO = new TagVO();
-            tagVO.setParentTagName(parentTagName);
-            tagVO.setChildTagNameList(childTagNameList);
-            result.add(tagVO);
-        }
-        return result;
-    }
-
-    @Override
-    @Transactional(rollbackFor = {BusinessException.class})
     public Boolean editTag(TagEditRequest tagEditRequest, HttpServletRequest request) {
         // 校验
         ThrowUtils.throwIf(Objects.isNull(tagEditRequest), StatusCode.PARAMS_ERROR, "修改标签参数不能为空");
@@ -141,10 +105,13 @@ public class TagServiceImpl extends ServiceImpl<TagMapper, Tag>
         Long parentId = tagEditRequest.getParentId();
         ThrowUtils.throwIf(id <= 0L, StatusCode.PARAMS_ERROR, "修改标签参数不合法！");
         ThrowUtils.throwIf(parentId < 0L, StatusCode.PARAMS_ERROR, "修改标签参数不合法！");
+        UserVO loginUser = userService.getLoginUser(request);
         Tag editTag = this.getById(id);
         ThrowUtils.throwIf(Objects.isNull(editTag), StatusCode.NOT_FOUND_ERROR, "要修改的标签不存在！");
-        // 管理员 || 自己
-        authManager.adminOrMe(editTag.getUserId(), request);
+        boolean isAdmin = ADMIN.equals(loginUser.getUserRole());
+        boolean isMe = editTag.getUserId().equals(loginUser.getId());
+        // 管理员 || 自己  可以修改
+        ThrowUtils.throwIf(!(isAdmin || isMe), StatusCode.NO_AUTH_ERROR, "你无权修改此标签!");
 
         LambdaUpdateWrapper<Tag> updateWrapper = new LambdaUpdateWrapper<>();
         // 修改tagName
